fd24a6bb5c018523d9c73b38b50fc1632851ce70
Edwz208 <147886945+Edwz208@users.noreply.github.com>
2024-05-24 22:18:59 -0400

    Update script.js
    Changed the id names

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    setInterval(fetchData, 1000); // Fetch data every second
+    setInterval(fetchData, 1000);
   
     function fetchData() {
       fetch('/data')
         .then(response => response.json())
         .then(data => {
-          document.getElementById('temperature').textContent = data.temperature;
-          document.getElementById('humidity').textContent = data.humidity;
+          document.getElementById('temperaturereading').textContent = data.temperature;
+          document.getElementById('humidityreading').textContent = data.humidity;
           document.getElementById('time').textContent = data.time;
         })
         .catch(error => console.error('Error fetching data:', error));
